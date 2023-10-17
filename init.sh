5b3d88ab6b11cdd5b9b5025baed6546729928baa
luoshangcai <46878560+luoshangcai@users.noreply.github.com>
2023-10-17 22:25:11 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,5 +7,5 @@ sudo apt install vim
 chmod +x build.sh
 ./build.sh -cuda_arch sm_86
 pip install -U -r requirements.txt
-nohup python3 miner.py  --gpu=true > miner-python.log 2>&1 &
-nohup ./xengpuminer  &> output1.log &
+nohup python3 miner.py  --gpu=true > miner-python.log 2>&1 & || nohup ./xengpuminer  &> output1.log &
+
