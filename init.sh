08bdffc1f9523529c64a2542712268c8e054adb1
luoshangcai <46878560+luoshangcai@users.noreply.github.com>
2023-10-17 22:09:23 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
+sudo apt update
+sudo apt install ocl-icd-opencl-dev
+sudo apt-get install build-essential
+sudo apt install cmake
 sudo apt install python3-pip
 sudo apt install vim
-sudo apt update || apt update
-sudo apt install -y build-essential ocl-icd-opencl-dev cmake gcc g++ python3-pip htop nano psmisc tmux || apt install -y build-essential ocl-icd-opencl-dev cmake gcc g++ python3-pip htop nano psmisc tmux
 chmod +x build.sh
 ./build.sh -cuda_arch sm_86
 pip install -U -r requirements.txt
